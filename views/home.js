3c469c3a0f2dd78b181ccdd27d3b700a8d6093c0
leib garber <steve.l.garber@gmail.com>
2015-06-23 22:31:22 +0300

    formatted table

--- views/home.js
+++ views/home.js
@@ -1,8 +1,29 @@
 'use strict';
 
 angular.module('mny')
-    .controller('HomeCtrl', function($scope, $http, $state, $stateParams){
+    .controller('HomeCtrl', function($scope, $http, $state, $stateParams, data){
 
+	data.read().then(function(pon){
+	    $scope.spends = pon;
+	});
+
+	$scope.formatDate = function(date){
+	    var monthNames = [
+		"January", "February", "March",
+		"April", "May", "June", "July",
+		"August", "September", "October",
+		"November", "December"
+	    ];
+
+	    var day = date.getDate();
+	    var monthIndex = date.getMonth();
+	    var year = date.getFullYear();
+
+	    console.log(day, monthNames[monthIndex], year);
+
+	    return monthNames[monthIndex]+' '+day+' '+year;
+	};
 	
     });
+
 
